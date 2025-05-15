690402299bb8507857f9842a0d7c1ef2259dea0d
rik prohaska <prohaska7@gmail.com>
2025-05-15 10:15:40 -0400

    MSAN detects uninitialized data that is written by the realpath function.
    Test project /home/rfp/tokubackup-may2025/tokubackup-msan-clang-18
        Start 47: realpath_error_injection
    1/1 Test #47: realpath_error_injection .........***Failed    0.74 sec
    Uninitialized bytes in strcmp at offset 0 inside [0x706000000000, 88)
    ==688300==WARNING: MemorySanitizer: use-of-uninitialized-value
        #0 0x7ffff7f8ff9c in tokubackup_create_backup /home/rfp/tokubackup-may2025/tokubackup/backup/backup.cc:370:13
        #1 0x555555628a30 in start_backup_thread_fun(void*) /home/rfp/tokubackup-may2025/tokubackup/backup/tests/backup_test_helpers.cc:159:13
        #2 0x7ffff769caa3 in start_thread nptl/pthread_create.c:447:8
        #3 0x7ffff7729c3b in clone3 misc/../sysdeps/unix/sysv/linux/x86_64/clone3.S:78
    
      Uninitialized value was created by a heap allocation
        #0 0x5555555c3026 in malloc (/home/rfp/tokubackup-may2025/tokubackup-msan-clang-18/tests/realpath_error_injection+0x6f026) (BuildId: d89b598bccfc4379147f4859f67833ed60cfbd08)
        #1 0x7ffff76b435e in strdup string/strdup.c:42:15
        #2 0x7ffff7646e86 in realpath_stk stdlib/canonicalize.c:409:9
        #3 0x7ffff7646e86 in realpath stdlib/canonicalize.c:431:18
        #4 0x555555621fb7 in my_realpath(char const*, char*) /home/rfp/tokubackup-may2025/tokubackup/backup/tests/realpath_error_injection.cc:117:16
        #5 0x7ffff7f39cde in call_real_realpath(char const*, char*) /home/rfp/tokubackup-may2025/tokubackup/backup/real_syscalls.cc:296:12
        #6 0x7ffff7f8fa79 in tokubackup_create_backup /home/rfp/tokubackup-may2025/tokubackup/backup/backup.cc:358:49
        #7 0x555555628a30 in start_backup_thread_fun(void*) /home/rfp/tokubackup-may2025/tokubackup/backup/tests/backup_test_helpers.cc:159:13
        #8 0x7ffff769caa3 in start_thread nptl/pthread_create.c:447:8
    
    SUMMARY: MemorySanitizer: use-of-uninitialized-value /home/rfp/tokubackup-may2025/tokubackup/backup/backup.cc:370:13 in tokubackup_create_backup
    Exiting
    
    MSAN requires all programs to be compiled with MSAN enabled including standard libraries.
    https://clang.llvm.org/docs/MemorySanitizer.html
    Unfortunately, we are running tests without the standard c libraries instrumented.
    
    We include a quick and dirty implementation of realpath used by the tests to instrument realpath and avoid the uninitialized data problem.

--- backup/real_syscalls.cc
+++ backup/real_syscalls.cc
@@ -236,6 +236,53 @@ mkdir_fun_t register_mkdir(mkdir_fun_t f) throw() {
     return r;
 }
 
+#if defined(__has_feature)
+#if __has_feature(memory_sanitizer)
+#define USE_MY_REALPATH 1
+#endif
+#endif
+
+#if USE_MY_REALPATH
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <limits.h>
+#include <string.h>
+char *my_msan_realpath(const char *path, char *out) {
+    assert(path != NULL);
+    if (out == NULL) {
+        out = (char *) malloc(PATH_MAX);
+        assert(out != NULL);
+    }
+    if (path[0] == '/') {
+        int r = snprintf(out, PATH_MAX, "%s", path);
+        assert(r >= 0 && r < PATH_MAX);
+    } else {
+        assert(getcwd(out, PATH_MAX) != NULL);
+        int n = strlen(out);
+        int r = snprintf(out+n, PATH_MAX-n, "/%s", path);
+        assert(r >= 0 && r < PATH_MAX-n);
+    }
+    return out;
+}
+
+static realpath_fun_t real_realpath = NULL;
+realpath_fun_t register_realpath(realpath_fun_t f) throw() {
+    if (real_realpath == NULL)
+        real_realpath = my_msan_realpath;
+    realpath_fun_t r = real_realpath;
+    real_realpath = f;
+    return r;
+}
+
+char *call_real_realpath(const char *pathname, char *result) throw() {
+    if (real_realpath == NULL)
+        real_realpath = my_msan_realpath;
+    return real_realpath(pathname, result);
+}
+
+#else
+
 static realpath_fun_t real_realpath = NULL;
 realpath_fun_t register_realpath(realpath_fun_t f) throw() {
     dlvsym_set(&real_realpath, "realpath", "GLIBC_2.3");
@@ -248,3 +295,5 @@ char *call_real_realpath(const char *pathname, char *result) throw() {
     dlvsym_set(&real_realpath, "realpath", "GLIBC_2.3");
     return real_realpath(pathname, result);
 }
+
+#endif
